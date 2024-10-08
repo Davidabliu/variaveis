ccdf6dddba5e043d3862971851680f1ff108f694
Davidabliu <davi.rafaelwandelbruckda.silva@escola.pr.gov.br>
2024-10-08 22:56:11 +0000

    8/10

--- index.js
+++ index.js
@@ -1,31 +1,60 @@
-const NomeCompleto = "Davi Wandelbruck";
-const Nascimento = "2005";
-const Idade = "19 anos";
-const Local = "São José Dos Pinhais"
+const prompt = require('prompt-sync');
+const entrada = prompt();
 
-console.log (`oi eu sou ${NomeCompleto}, meu ano de nascimento é ${Nascimento} e atualmente tenho ${Idade}, moro em ${Local}`);
+const nomeCompleto = "Davi Wandelbruck";
+const anoNascimento = 2005;
+let anoAtual = 2024;
+let idade = 19;
 
-const listaDeViagens = new Array
-  `Madrid na Espanha`,
-  `Portugal`,
-  `Estados Unidos`,
-  `Sidney na Australia`,
-  `Chile`
-)
+console.log("Olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " minha idade atual é "  + idade + " e sou nascido no ano " + anoNascimento );
 
-console.log (listaDeViagens);
+anoAtual = anoAtual + 1;
+idade = idade +1;
 
-console.log(listaDeViagens [0]);
-listaDeViagens.push ("Coronel Vivida");
-console.log(listaDeViagens);
-listaDeViagens.splice(1,2);
-console.log(listaDeViagens);
+console.log (`oi eu sou ${nomeCompleto} meu ano de nascimento é ${anoNascimento} e estamos no ano ${anoAtual} e minha idade é ${idade}`);
 
-const idadeComprador = 19; 
+const loginCerto = "Davi Dabliu";
+const senhaCerta = "654321";
+console.log("-----------------");
+console.log("--AGENCIA DE VIAGENS--");
+console.log("-----------------");
+console.log("Faça seu login para prosseguir");
 
-if(idadeComprador < 18){
-  console.log("infelizmente o sistema não disponibiliza venda para menores de idade")
+var login = entrada("digite o usuário: ");
+var senha = entrada("digite sua senha: ");
+
+while(login != loginCerto){
+  console.log("login Incorreto");
+  login = entrada ("Digite o usuário novamente: ");
 }
-if(idadeComprador >= 18){
-  console.log ("você poderá prosseguir sua compra")
+while(senha != senhaCerta){
+  console.log("senha Incorreta");
+  senha = entrada ("Insira a senha novamente: ");
+}
+
+  const listaDeViagens = new Array(
+'Japão',
+'Londres',
+'paris',
+'Inglaterra',
+) ;
+
+console.log(listaDeViagens) ;
+
+console.log(listaDeViagens[4]);
+listaDeViagens.push("Coronel Vivida");
+console.log(listaDeViagens);
+
+listaDeViagens.splice(3,1);
+
+var nomeComprador = entrada("Qual seu nome?");
+var idadeComprador = entrada("Qual sua idade?");
+
+if (idadeComprador<18){
+
+console.log(`Olá senhor(a) ${nomeComprador} infelizmente você não poderá viajar conosco`);
+  console.log(`Já que sua idade é ${idadeComprador}`)
+};
+if (idadeComprador>=18){
+  console.log(`Párabens senhor(a) ${nomeComprador} ,você é apto à viajar,para onde deseja ir? Os destinos são ${listaDeViagens}`);
 }
